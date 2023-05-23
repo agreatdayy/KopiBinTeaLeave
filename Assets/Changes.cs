d313d06a41a8e40d825a3bddd1d6805dc83b66b5
Tee Chu Jie <e0968553@u.nus.edu>
2023-05-24 00:39:05 +0800

    make changes

---
+++ Assets/Changes.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changes : MonoBehaviour
+{
+    void OnMouseDown() {
+        Debug.Log("Hello! I'm changes :)");
+    }
+}
