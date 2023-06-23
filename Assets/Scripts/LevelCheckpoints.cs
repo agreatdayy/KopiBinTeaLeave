e52ecbb018bac8fedca7e9932864f117e95aa0d6
Tee Chu Jie <e0968553@u.nus.edu>
2023-06-23 22:01:19 +0800

    edit respawn delay and level checkpoints

--- Assets/Scripts/LevelCheckpoints.cs
+++ Assets/Scripts/LevelCheckpoints.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class LevelCheckpoints : MonoBehaviour
 {
     [SerializeField] string level;
+    //BoxCollider2D myBoxCollider;
+
+    //void Start() {
+    //    myBoxCollider = GetComponent<BoxCollider2D>();
+    //}
 
     public void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
+        //if (myBoxCollider.IsTouchingLayers(LayerMask.GetMask("Player"))) { 
             PlayfabManager.Instance.SaveLevel(level);
             Debug.Log("Level saved successfully!");
         }
